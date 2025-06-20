85f3e9c30b4377a9538d95702124896f81a7ecac
shripad-kasar <shripadkasar@gmail.com>
2025-06-20 21:45:50 +0530

    post Branch code

--- CrudOnGit/src/main/java/com/app/controller/StudentController.java
+++ CrudOnGit/src/main/java/com/app/controller/StudentController.java
@@ -1,8 +1,10 @@
 package com.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.model.Student;
 import com.app.service.StudentService;
 
 @RestController
@@ -10,4 +12,9 @@ public class StudentController {
 
 	@Autowired
 	StudentService ssi;
+	
+	public Student saveData(@RequestBody Student s) {
+		Student save = ssi.save(s);
+		return save;
+	}
 }
--- CrudOnGit/src/main/java/com/app/serviceIMPL/StudentServiceIMPL.java
+++ CrudOnGit/src/main/java/com/app/serviceIMPL/StudentServiceIMPL.java
@@ -3,6 +3,7 @@ package com.app.serviceIMPL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.app.model.Student;
 import com.app.repository.StudentRepo;
 import com.app.service.StudentService;
 
@@ -11,4 +12,10 @@ public class StudentServiceIMPL implements StudentService {
 
 	@Autowired
 	StudentRepo sr;
+
+	@Override
+	public Student save(Student s) {
+		Student save = sr.save(s);
+		return save;
+	}
 }
