4a6363f1486369ef7e3fec6a93db1a4fc5c12f15
shripad-kasar <shripadkasar@gmail.com>
2025-06-20 22:18:48 +0530

    add put method

--- CrudOnGit/src/main/java/com/app/controller/StudentController.java
+++ CrudOnGit/src/main/java/com/app/controller/StudentController.java
@@ -1,8 +1,12 @@
 package com.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.model.Student;
 import com.app.service.StudentService;
 
 @RestController
@@ -10,4 +14,12 @@ public class StudentController {
 
 	@Autowired
 	StudentService ssi;
+	
+	@PutMapping("/update/{id}")
+	public Student updateData(@RequestBody Student s,@RequestParam ("id") int id) {
+	
+		Student update = ssi.update(s,id);
+		return update;
+		
+	}
 }
--- CrudOnGit/src/main/java/com/app/serviceIMPL/StudentServiceIMPL.java
+++ CrudOnGit/src/main/java/com/app/serviceIMPL/StudentServiceIMPL.java
@@ -1,8 +1,11 @@
 package com.app.serviceIMPL;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.app.model.Student;
 import com.app.repository.StudentRepo;
 import com.app.service.StudentService;
 
@@ -11,4 +14,14 @@ public class StudentServiceIMPL implements StudentService {
 
 	@Autowired
 	StudentRepo sr;
+
+	@Override
+	public Student update(Student s, int id) {
+		Optional<Student> findById = sr.findById(id);
+		if(findById.isPresent()) {
+			Student save = sr.save(s);
+			return save;
+		}
+		return null;
+	}
 }
