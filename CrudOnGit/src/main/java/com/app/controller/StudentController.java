bee4608e5092f4b67c9e48be21973fdd090e338f
shripad-kasar <shripadkasar@gmail.com>
2025-06-20 21:57:27 +0530

    get branch data

--- CrudOnGit/src/main/java/com/app/controller/StudentController.java
+++ CrudOnGit/src/main/java/com/app/controller/StudentController.java
@@ -1,8 +1,12 @@
 package com.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.model.Student;
 import com.app.service.StudentService;
 
 @RestController
@@ -10,4 +14,13 @@ public class StudentController {
 
 	@Autowired
 	StudentService ssi;
+	
+	@GetMapping("/getData/{id}")
+	public Student getData(@RequestParam ("id") int id) {
+		
+		Student data = ssi.getData(id);
+		return data;
+		
+		
+	}
 }
--- CrudOnGit/src/main/java/com/app/service/StudentService.java
+++ CrudOnGit/src/main/java/com/app/service/StudentService.java
@@ -1,5 +1,9 @@
 package com.app.service;
 
+import com.app.model.Student;
+
 public interface StudentService {
+
+	public Student getData( int id);
 
 }
